a7b1abdd1f4a9b916fc209a8731359a7e7b77435
antoniomerlin <gauravsinghbr@hotmail.com>
2014-09-03 10:11:05 +0530

    cronus server

---
+++ proto/cronus/src/com/Cronus/Server/Cronus_Server.java
@@ -0,0 +1,13 @@
+package com.Cronus.Server;
+
+public class Cronus_Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
